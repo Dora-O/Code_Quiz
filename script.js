ab19be395a70eb203d1cbcfe247a5358993a7630
Dora O <dorozco2390@utexas.edu>
2021-01-15 13:25:09 -0600

    created variables for divs to be displayed on screen

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+//startbutton var
 var startBtn = document.querySelector("#start-button");
+//scorebutton var
 var scoreBtn = document.querySelector("#get-score");
+//question var to display on screen
+var questionDisplay = document.querySelector("#questionDisplay");
+//choices var to display on screen
+var choices = document.querySelector("#choices");
+//correct answer var 
+var result = document.querySelector("#result");
 var score = 0;
 var timeLeft = 0;
 var timer ;
